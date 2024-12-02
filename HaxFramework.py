7f683e8a9f9f1655aa2dc1b7c815ca0b4088e8e0
haxerr_ <148996577+haxerr9@users.noreply.github.com>
2024-12-02 20:16:55 +0300

    Add files via upload

---
+++ HaxFramework.py
@@ -0,0 +1,43 @@
+import os
+import subprocess
+import pyfiglet
+
+bannerText = "       Hax Framework"
+banner = pyfiglet.figlet_format(bannerText)
+
+hMenu = """
+
+        --- Hax Framework Help Menu ---
+
+Scan Help Menu:
+    Ex. Usage:
+        -> scan [IP]
+
+"""
+
+print("Welcome To \n", banner, "\nConsole!")
+print(hMenu)
+
+
+def scan(IP):
+    command = f"nmap {IP} -sCV -O -A -T4 -v"
+    result = os.system(command)
+
+    print(result)
+
+print(" --> HaxFramework Loaded. Type 'help' for help menu. ")
+
+while True:
+    cmd = input("\nHaxFramework_0.1 >>> ")
+
+    if cmd == "help" or cmd == "Help":
+        print(hMenu)
+
+    elif cmd.startswith("scan "):
+        cmdParts = cmd.split()
+        
+        ipAddr = cmdParts[1]
+        scan(ipAddr)
+
+    else:
+        print("\n[!] Not A Valid Command !")
